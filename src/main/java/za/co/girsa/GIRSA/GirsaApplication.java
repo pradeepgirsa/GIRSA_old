16abb78f193d1bd3d3f261b6e420cf541c830a1b
GIRSA <pradeepreddy232@gmail.com>
2018-08-02 17:14:55 +0200

    First commit

---
+++ src/main/java/za/co/girsa/GIRSA/GirsaApplication.java
@@ -0,0 +1,12 @@
+package za.co.girsa.GIRSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirsaApplication.class, args);
+	}
+}
---
+++ src/test/java/za/co/girsa/GIRSA/GirsaApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.girsa.GIRSA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GirsaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
